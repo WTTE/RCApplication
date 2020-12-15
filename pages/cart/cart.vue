5baeb6319ba927ab7546cdb0bcc51de85792d2e6
mazg1987 <mazg1987@163.com>
2020-12-15 18:27:38 +0800

    12.15

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,36 +1,34 @@
 <template>
 	<view class="top-nav">
-		<view class="top-nav-bar" >
-			<view @click="chenked(1)" >全部</view>
-			<view @click="chenked(2)" >待付款</view>
-			<view @click="chenked(3)" >待入住</view>
-			<view @click="chenked(4)" >已入住</view>
-			<view @click="chenked(5)" >已完成</view>
-			<view @click="chenked(6)" >已取消</view>
+		<view class="top-nav-bar">
+			<view class="item" :class="{ active: isActive == 1 }" @click="chenked(1)">全部</view>
+			<view class="item" :class="{ active: isActive == 2 }" @click="chenked(2)">待付款</view>
+			<view class="item" :class="{ active: isActive == 3 }" @click="chenked(3)">待入住</view>
+			<view class="item" :class="{ active: isActive == 4 }" @click="chenked(4)">已入住</view>
+			<view class="item" :class="{ active: isActive == 5 }" @click="chenked(5)">已完成</view>
+			<view class="item" :class="{ active: isActive == 6 }" @click="chenked(6)">已取消</view>
 		</view>
 		<view>
-			<image src="../../static/icons/捕获1.png"></image>
+			<image @click="goHuiYuan()" src="../../static/icons/任享会员688+45天会期-通用通栏.png"></image>
 		</view>
-		<view v-if="isactive==1">
+		<view class="nav_item" v-if="isActive==1">
 			<Goods></Goods>
 		</view>
-		<view v-if="isactive==2">
+		<view class="nav_item" v-if="isActive==2">
 			<List></List>
 		</view>
-		<view v-if="isactive==3">
+		<view class="nav_item" v-if="isActive==3">
 			<Next></Next>
 		</view>
-		<view v-if="isactive==4">
+		<view class="nav_item" v-if="isActive==4">
 			<YiRu></YiRu>
 		</view>
-		<view v-if="isactive==5">
+		<view class="nav_item" v-if="isActive==5">
 			<WanChen></WanChen>
 		</view>
-		<view v-if="isactive==6">
+		<view class="nav_item" v-if="isActive==6">
 			<QuXiao></QuXiao>
 		</view>
-
-
 	</view>
 </template>
 
@@ -38,23 +36,28 @@
 	import Goods from '@/components/dingdan/goods.vue'
 	import List from '@/components/dingdan/list.vue'
 	import Next from '@/components/dingdan/next.vue'
-	import YiRu from  '@/components/dingdan/yiru.vue'
-	import WanChen from  '@/components/dingdan/wanchen.vue'
-	import QuXiao from  '@/components/dingdan/quxiao.vue'
+	import YiRu from '@/components/dingdan/yiru.vue'
+	import WanChen from '@/components/dingdan/wanchen.vue'
+	import QuXiao from '@/components/dingdan/quxiao.vue'
+
 	export default {
 		data() {
 			return {
-				isactive: 1
+				isActive: 1,
 			}
-			
 		},
 		onLoad() {
-             
+
 		},
 		methods: {
 			chenked(type) {
-				this.isactive = type;
-			}
+				this.isActive = type;
+			},
+			goHuiYuan() {
+				uni.navigateTo({
+					url: "/pages/vip/vip"
+				})
+			},
 		},
 		components: {
 			Goods,
@@ -70,19 +73,45 @@
 
 <style>
 	.top-nav {
-	  width: 750rpx;
-	  position: fixed;
-	  top: 10rpx;
+		width: 750rpx;
+		position: fixed;
 	}
-	
 	.top-nav-bar {
 		border: 2px solid white;
 		display: flex;
-		justify-content: space-around;
+		height: 44px;
+		justify-content: space-between;
 		font-size: 13px;
-		padding: 10px 0;
 		background-color: #fff;
-		border-top: 1px solid #e5e5e5;
+		overflow: scroll;
+		color: #353b48;
+		border-top: 0px solid #e5e5e5;
+	}
+	.nav-item {
+		flex: 1;
+	}
+	.item {
+		width: 70px;
+		text-align: center;
+		line-height: 33px;
+		height: 33px;
+	}
+	.active {
+		color: #ff0000;
+		font-weight: 550;
+		position: relative;
+	}
+	.active::after {
+		content: '';
+		position: absolute;
+		top: 25px;
+		width: 90rpx;
+		height: 8rpx;
+		background-color: red;
+		left: 0px;
+		right: 0px;
+		bottom: 0px;
+		margin: auto;
 	}
 	image {
 		width: 750rpx;
---
+++ pages/vip/vip.vue
@@ -0,0 +1,64 @@
+<template>
+	<view>
+			<view>
+				<image src="https://image.ructrip.com/ructrip/1597395391331/1280元如程详情页更新-官方.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670328318/1280元如程详情页更新-官方_03.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670352795/1280元如程详情页更新-官方_04.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1593312867801/如程详情页更新-官方_05.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670361565/1280元如程详情页更新-官方_06.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670364380/1280元如程详情页更新-官方_07.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670367646/1280元如程详情页更新-官方_08.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670370975/1280元如程详情页更新-官方_09.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670386417/1280%E5%85%83%E5%A6%82%E7%A8%8B%E8%AF%A6%E6%83%85%E9%A1%B5%E6%9B%B4%E6%96%B0-%E5%AE%98%E6%96%B9_10.jpg"
+				 mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670408037/1280元如程详情页更新-官方_15.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670432771/1280元如程详情页更新-官方_17.jpg" mode="widthFix"></image>
+				<image src="https://image.ructrip.com/ructrip/1585670437598/1280元如程详情页更新-官方_20.jpg" mode="widthFix"></image>
+			<view class="bottom">
+				<button class="left" @click="tan">加入会员</button>
+				
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	
+
+	export default {
+		data() {
+			return {
+				}
+		},
+		
+		methods: {
+			
+		}
+
+	}
+</script>
+
+<style scoped lang="less">
+	image {
+		width: 100%;
+	}
+	.bottom{
+		display: flex;
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		background-color: #FFFFFF;
+		box-shadow: 0 2px;
+		padding: 16rpx 0;
+		button{
+			width: 325rpx;
+			height: 90rpx;
+			background-color: #fc6315;
+			font-size: 36rpx;
+			font-weight: 700;
+			color: #FFFFFF;
+			margin-left: auto;
+			margin-right: auto;
+		}
+		}
+</style>
