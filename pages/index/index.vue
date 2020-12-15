ed480256d5f0ec66989b6950049f72f525efdb5f
wangsonghan123 <1245652684@qq.com>
2020-12-15 21:55:49 +0800

    代码修改了

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,23 +2,32 @@
 	<view class="home">
 		<Preferent></Preferent>
 		<Search></Search>
-		<swiper>
-			<swiper-item >
-				<image src="https://image.ructrip.com/ructrip/1564132913649/_DSC2483.jpg?imageView2/1/w/672/h/430" ></image>
-			</swiper-item>
-			<swiper-item >
-				<image src="https://image.ructrip.com/ructrip/1564132913621/_DSC2486.jpg?imageView2/1/w/672/h/430" ></image>
-			</swiper-item>
-			<swiper-item >
-				<image src="https://image.ructrip.com/ructrip/1564132913658/_DSC2482.jpg?imageView2/1/w/672/h/430" ></image>
-			</swiper-item>
-		</swiper>
+
+		<view class="content1">
+			<swiper :interval="4500" circular>
+				<swiper-item v-for="item in a">
+					<image :src="item"></image>
+				</swiper-item>
+			</swiper>
+			<view class="xiazhou">
+				<view class="title1">
+					<text class="title" v-for="item in title">{{item}}</text>
+				</view>
+				<view class="word1"><text>{{this.word1}}</text></view>
+				<view class="word2">
+					<text>{{this.addr}}</text>
+					<text>{{this.word2}}</text>
+				</view>
+			</view>
+		</view>
+
 	</view>
 </template>
 
 <script>
 	import Search from '../../components/search.vue'
 	import Preferent from '../../components/preferent.vue'
+
 	import {
 		myRequestPost
 	} from "@/utils/request.js"
@@ -30,7 +39,12 @@
 		},
 		data() {
 			return {
-			res:""
+				res: "",
+				title: "",
+				word1: "",
+				word2: "",
+				addr: "",
+				a: []
 			}
 		},
 		onLoad() {
@@ -47,23 +61,74 @@
 					"timestamp": 1607426297000,
 					"sign": "48805753110D7AB46CA4EE8E34737E9F"
 				})
-				this.res = result.respData.storeImage
-				console.log(result.respData.storeImage);
+
+				this.title = result.respData.sellingLabel.split(",")
+				this.word1 = result.respData.subTitle
+				this.addr = result.respData.city
+				this.word2 = result.respData.storeName
+				this.a = result.respData.storeImage.split(",")
+				console.log(a)
+				console.log(result);
+				console.log(result.respData.city)
 			}
 		}
 	}
 </script>
 
 <style lang="scss">
-	.home{
-		swiper{
-			width: 690rpx;
-			height: 390rpx;
-			margin: auto;
-			image{
-				height: 100%;
-				width: 100%;
-			}
+	.content1 {
+		border: 1rpx solid #eee;
+		height: 650rpx;
+		width: 90%;
+		margin: auto;
+	
+
+	.title1 {
+		display: flex;
+		margin: auto;
+	
+
+	}
+
+	swiper {
+		width: 100%;
+		height: 390rpx;
+
+		image {
+			height: 100%;
+			width: 100%;
 		}
+	}
+
+	.title {
+		height: 50rpx;
+		font-size: 25rpx;
+		line-height: 50rpx;
+		text-align: center;
+		display: block;
+		margin-right: 10rpx;
+		margin-top: 30rpx;
+		padding-left: 8rpx;
+		padding-right: 8rpx;
+		background-color: #E5E5E5;
+		opacity: 0.6;
+	}
+
+	.word1 {
+		text {
+			display: block;
+			margin-top: 30rpx;
+			margin-right: 20rpx;
+			font-size: 45rpx;
+			opacity: 0.6;
+		}
+
+	}
+
+	.word2 {
+		margin-top: 20rpx;
+		margin-right: 20rpx;
+		opacity: 0.4;
+	}
 	}
 </style>
