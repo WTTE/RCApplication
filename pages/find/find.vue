dce4adba5d8e8ef55ec5ba2277d44921d6bb54b3
wangsonghan123 <1245652684@qq.com>
2020-12-15 08:56:36 +0800

    -m修改

---
+++ pages/find/find.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		这是发现页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
