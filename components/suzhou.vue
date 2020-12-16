4827c88531f16144ea61bfd3b191537428c0f0c8
wangsonghan123 <1245652684@qq.com>
2020-12-16 21:49:57 +0800

    16号修改

--- components/suzhou.vue
+++ components/suzhou.vue
@@ -1,45 +1,108 @@
 <template>
-	<view>
-		<swiper :interval="3000" circular>
-			<swiper-item v-for="item in b">
-				<image:src="item"></image>
-			</swiper-item>
-			
-		</swiper>
+	<view class="contents">
+		<view class="content1">
+			<swiper :interval="4500" circular>
+				<swiper-item v-for="item in res.storeImage.split(',')" :key="item">
+					<image :src="item" mode="widthFix"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="word">
+			<view class="laber">
+				<text class="reserve">{{res.storeReserve}}</text>
+				<text v-for="item in res.sellingLabel.split(',')">{{item}}</text>
+			</view>
+			<view class="title">
+				{{res.subTitle}}
+			</view>
+			<view class="name">
+				<text>{{res.storeName}}</text>
+				<text>特色度假酒店</text>
+			</view>
+		</view>
+
 	</view>
 </template>
 
 <script>
-	import{
+	import {
 		myRequestPost
-	} from"@/utils/request.js"
+	} from "@/utils/request.js"
 	export default {
+		props: ['res', 'pageNum'],
 		data() {
 			return {
-			b:[]	
-			};
-		},
-		onLoad(){
-			this.getword1();
-		},
-		methods:{
-			async getword1(){
-				let result =await myRequestPost("/sojo.equity.store.detail.v.two",{
-					"storeNo": "401000001935",
-					"client": "applets",
-					"mobileBrand": "microsoft",
-					"mobileModel": "microsoft",
-					"osVersion": "Windows 10 x64",
-					"timestamp": 1607426297000,
-					"sign": "48805753110D7AB46CA4EE8E34737E9F"
-				})
-				this.b = result.respData.storeImage.split(',')
-				console.log(result)
+
 			}
+		},
+
+		methods: {
+
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.contents {
+		width: 93%;
+		margin: auto;
+		margin-top: 30rpx;
+		border: 2rpx solid #eeee;
 
+		.content1 {
+			height: 440rpx;
+
+			swiper {
+				height: 196px;
+
+				image {
+					width: 348px;
+					height: 210px;
+
+				}
+			}
+
+		}
+
+		.word {
+			width: 96%;
+			margin: auto;
+
+			.laber {
+
+				display: flex;
+
+				.reserve {
+					color: orangered;
+				}
+
+				text {
+					display: block;
+
+
+					font-size: 15rpx;
+					background-color: #eee;
+					margin-right: 8rpx;
+
+				}
+			}
+
+			.title {
+				margin-top: 30rpx;
+				display: block;
+				font-size: 42rpx;
+				font-weight: 300;
+				margin-bottom: 20rpx;
+			}
+		}
+
+		.name {
+			display: flex;
+
+			text {
+				display: block;
+				margin-bottom: 30rpx;
+			}
+		}
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,25 +2,11 @@
 	<view class="home">
 		<Preferent></Preferent>
 		<Search></Search>
-		<Suzhou></Suzhou>
-		<view class="content1">
-			<swiper :interval="4500" circular>
-				<swiper-item v-for="item in a">
-					<image :src="item"></image>
-				</swiper-item>
-			</swiper>
-			<view class="xiazhou">
-				<view class="title1">
-					<text class="title" v-for="item in title">{{item}}</text>
-				</view>
-				<view class="word1"><text>{{this.word1}}</text></view>
-				<view class="word2">
-					<text>{{this.addr}}</text>
-					<text>{{this.word2}}</text>
-				</view>
-			</view>
+		<view v-if="isActive == 1" v-for="item in number" :key="item">
+			<Suzhou :res="res[item]" :pageNum="pageNum"></Suzhou>
 		</view>
-
+		<uni-load-more v-if="!flag" :status="'loading'"></uni-load-more>
+		<uni-load-more v-else :status="'noMore'"></uni-load-more>
 	</view>
 </template>
 
@@ -28,6 +14,7 @@
 	import Search from '../../components/search.vue'
 	import Preferent from '../../components/preferent.vue'
 	import Suzhou from '../../components/suzhou.vue'
+	import uniLoadMore from "@/components/uni-ui/uni-load-more/uni-load-more.vue"
 	import {
 		myRequestPost
 	} from "@/utils/request.js"
@@ -39,101 +26,62 @@
 		},
 		data() {
 			return {
-				res: "",
-				title: [],
-				word1: "",
-				word2: "",
-				addr: "",
-				a: []
+				isActive:1,
+				res: [],
+				flag: false,
+				pageNum: 1,
+				pageSize:6,
+				number: [0, 1, 2, 3, 4, 5]
+
+
 			}
 		},
 		onLoad() {
 			this.getword();
 		},
 		methods: {
+		/* 	chenked(type) {
+				this.isActive = type;
+				this.pageNum = 1;
+				
+			}, */
 			async getword() {
-				let result = await myRequestPost("/sojo.equity.store.detail.v.two", {
-					"storeNo": "401000001935",
+				let result = await myRequestPost("/sojo.equity.home.listIndexCityMenuRecommend", {
+					"pageNum": this.pageNum,
+					"pageSize": 3,
+					"cityCode": 320500,
+					"platFrom": 10,
+					"memberCityCode": "320200",
+					"storeNoListStr": "",
 					"client": "applets",
 					"mobileBrand": "microsoft",
 					"mobileModel": "microsoft",
 					"osVersion": "Windows 10 x64",
-					"timestamp": 1607426297000,
-					"sign": "48805753110D7AB46CA4EE8E34737E9F"
-				})
+					"timestamp": 1608105167000,
+					"sign": "2BD4F2E388596CE4EB209B0C440BD3EF"
+				});
+				this.res = [...this.res, ...result.respData.list]
 
-				this.title = result.respData.sellingLabel.split(",")
-				this.word1 = result.respData.subTitle
-				this.addr = result.respData.city
-				this.word2 = result.respData.storeName
-				this.a = result.respData.storeImage.split(",")
-				console.log(result.respData);
+				console.log(this.res, "11111111")
+				console.log(this.res[0].storeImage)
+				console.log(this.res[0], "0000000000")
 
-			}
-		}
+			},
+			onReachBottom() {
+				this.pageNum++;
+				if (this.pageNum <= 6) {
+					this.getword();
+
+
+				} else {
+					//没有更多数据了
+					this.flag = true;
+				}
+			},
+		},
 	}
 </script>
 
 <style lang="scss">
-	.content1 {
-		border: 1rpx solid #eee;
-		height: 650rpx;
-		width: 90%;
-		margin: auto;
-		margin-top: 30rpx;
 
-		.xiazhou {
-			width: 700rpx;
-			margin-left: 30rpx;
-		}
-
-
-		.title1 {
-			display: flex;
-			margin: auto;
-
-
-		}
-
-		swiper {
-			width: 100%;
-			height: 390rpx;
-
-			image {
-				height: 100%;
-				width: 100%;
-			}
-		}
-
-		.title {
-			height: 50rpx;
-			font-size: 25rpx;
-			line-height: 50rpx;
-			text-align: center;
-			display: block;
-			margin-right: 10rpx;
-			margin-top: 30rpx;
-			padding-left: 8rpx;
-			padding-right: 8rpx;
-			background-color: #E5E5E5;
-			opacity: 0.6;
-		}
-
-		.word1 {
-			text {
-				display: block;
-				margin-top: 30rpx;
-				margin-right: 20rpx;
-				font-size: 45rpx;
-				opacity: 0.6;
-			}
-
-		}
-
-		.word2 {
-			margin-top: 20rpx;
-			margin-right: 20rpx;
-			opacity: 0.4;
-		}
-	}
 </style>
